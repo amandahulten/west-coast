14542d1b32a2f74e05a0eed600a8dfd124e77f69
amandahulten <amahul1029@skola.goteborg.se>
2022-04-27 12:00:23 +0200

    continued on fetch api

--- src/App.js
+++ src/App.js
@@ -4,22 +4,49 @@ import Nav from "./components/Nav";
 import stromstadimg from "./images/stromstad.png";
 
 function App() {
-  const [data, setData] = useState(null);
-  const emojis = ["☀️", "☁️"];
+  const [data, setData] = useState([]);
+  // const emojis = ["☀️", "☁️"];
 
   //  const [loading, setLoading] = useState(true);
   //  const [error, setError] = useState(null);
 
   useEffect(() => {
-    fetch(
-      `https://opendata-download-metfcst.smhi.se/api/category/pmp3g/version/2/geotype/point/lon/11.61272/lat/58.242343/data.json`
-    )
-      .then((response) => response.json())
-      .then((entries) => setData(entries.timeSeries))
-      .catch((err) => {
-        console.log(err.message);
-      });
+    const cities = [
+      {
+        city: "Strömstad",
+        lon: 11.61272,
+        lat: 58.242343,
+      },
+      {
+        city: "Göteborg",
+        lon: 11.97456,
+        lat: 57.70887,
+      },
+      // {
+      //   city: "Strömstad",
+      //   lon: 10,
+      //   lat: 16,
+      // },
+    ];
+
+    for (let index = 0; index < cities.length; index++) {
+      const city = cities[index];
+
+      fetch(
+        `https://opendata-download-metfcst.smhi.se/api/category/pmp3g/version/2/geotype/point/lon/${city.lon}/lat/${city.lat}/data.json`
+      )
+        .then((response) => response.json())
+        .then((entries) => {
+          console.log("city", city);
+          setData((prevData) => [...prevData, entries.timeSeries]);
+        })
+        .catch((err) => {
+          console.log(err.message);
+        });
+    }
   }, []);
+
+  console.log(data);
 
   return (
     <div>
@@ -27,13 +54,9 @@ function App() {
       <div className="container">
         <Card
           src={stromstadimg}
-          weather={
-            data &&
-            data
-              .slice(0, 1)
-              .map((item, i) => <p key={i}>{item.parameters[10].values[0]}</p>)
-          }
-          header="Hejsan"
+          weather={data}
+          header={cities[0].city}
+          alt="Bild"
         />
       </div>
     </div>
--- src/Components/Card/index.js
+++ src/Components/Card/index.js
@@ -4,10 +4,16 @@ import styles from "./Card.module.scss";
 const Card = (props) => {
   return (
     <div className={styles.container}>
-      <h2>{props.weather}</h2>
-      <h1>{props.header}</h1>
-      <img src={props.src} />
-      <p>{props.emoji}</p>
+      <div className={styles.cardContainer}>
+        <h2>
+          {props.weather.slice(0, 1).map((item, i) => (
+            <p key={i}>{item.parameters[10].values[0]}</p>
+          ))}
+        </h2>
+        <h1>{props.header}</h1>
+        <img src={props.src} alt={props.alt} />
+        <p>{props.emoji}</p>
+      </div>
     </div>
   );
 };
