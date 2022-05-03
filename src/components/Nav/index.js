415b783330bd6252493cad03bb91f5ffd0548efe
sofiaronnkvist <89849180+sofiaronnkvist@users.noreply.github.com>
2022-05-03 13:49:33 +0200

    Added styling.

--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -6,9 +6,6 @@ const Nav = () => {
           <div>
             <h1>VV</h1>
           </div>
-          <div>
-              <img src="./assets/hamburger.png" alt="hamburger"/>
-          </div>
       </div>
     );
   };
