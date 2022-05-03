70c121c9c2239063c4dce2e740fd246bceabc2bc
sofiaronnkvist <89849180+sofiaronnkvist@users.noreply.github.com>
2022-05-03 13:34:30 +0200

    Added new card index file.

---
+++ src/components/Card/index.js
@@ -0,0 +1,17 @@
+import React from "react";
+import styles from "./Card.module.scss";
+
+const Card = (props) => {
+  return (
+    <div className={styles.container}>
+      <div className={styles.cardContainer}>
+        <h2>{props.weather}°C</h2>
+        <h1>{props.header}</h1>
+        <img src={props.src} alt={props.alt} />
+        <p>{props.emoji}</p>
+      </div>
+    </div>
+  );
+};
+
+export default Card;
